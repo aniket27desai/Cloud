8a238101d2a4dd3f12b556c23daaccef442544b0
aniket27desai <100474854+aniket27desai@users.noreply.github.com>
2022-03-13 10:19:11 +0530

    Adding
    Spring Cloud

---
+++ DigitalKYCCloud/src/main/java/com/sil/cloud/ws/DigitalKycCloudApplication.java
@@ -0,0 +1,13 @@
+package com.sil.cloud.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalKycCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalKycCloudApplication.class, args);
+	}
+
+}
